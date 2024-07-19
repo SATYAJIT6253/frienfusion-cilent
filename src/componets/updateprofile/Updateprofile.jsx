62211d13724697a2d28f332e71bd13ff5c1a42d2
SATYAJIT6253 <119771949+SATYAJIT6253@users.noreply.github.com>
2024-07-19 07:58:02 +0530

    update the updateprofile page

--- src/componets/updateprofile/Updateprofile.jsx
+++ src/componets/updateprofile/Updateprofile.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./updateprofile.scss";
-import userimg from "../../images/avtar.png";
 import { useDispatch, useSelector } from "react-redux";
 import { showToast, updateprofile } from "../../Redux/slices/appConfigure";
 import { axiosClient } from "../../pages/utils/axiosCilent";
-import { KEY_ACESS_TOKEN, removeItem } from "../../pages/utils/localStoragemanager";
+import {
+  KEY_ACESS_TOKEN,
+  removeItem,
+} from "../../pages/utils/localStoragemanager";
 import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
 import { TOAST_FAILURE } from "../../App";
-import { BsCardImage } from "react-icons/bs";
 function Updateprofile() {
   const myprofile = useSelector((state) => state.appconfigreducer.myProfile);
   const [name, setname] = useState("");
@@ -19,8 +19,7 @@ function Updateprofile() {
   function handleImageChange(e) {
     const file = e.target.files[0];
     const fileReader = new FileReader();
-    if (file) 
-    {
+    if (file) {
       fileReader.readAsDataURL(file);
       fileReader.onload = () => {
         if (fileReader.readyState === fileReader.DONE) {
@@ -30,27 +29,30 @@ function Updateprofile() {
       };
     }
   }
-  function submithandeler(e)
-  {
-      e.preventDefault();
-      dispatch(updateprofile({
-        name,bio,Userimg
-      }))
-
+  function submithandeler(e) {
+    e.preventDefault();
+    dispatch(
+      updateprofile({
+        name,
+        bio,
+        Userimg,
+      })
+    );
   }
   async function deleteaccounthandeler() {
-      try {
-         
-          await axiosClient.delete('/user/');
-          removeItem(KEY_ACESS_TOKEN);
-          navigate('/signup');
-          dispatch(showToast({
-            type:TOAST_FAILURE,
-            message:"account deleted sucessfully"
-          }))
-      } catch (error) {
-        console.log(error);
-      }
+    try {
+      await axiosClient.delete("/user/");
+      removeItem(KEY_ACESS_TOKEN);
+      navigate("/signup");
+      dispatch(
+        showToast({
+          type: TOAST_FAILURE,
+          message: "account deleted sucessfully",
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
   useEffect(() => {
     setname(myprofile?.name);
@@ -58,51 +60,52 @@ function Updateprofile() {
     setUserImg(myprofile?.avatar?.url);
   }, [myprofile]);
   return (
-    <div className="updateprofile">
-      <div className="container">
-        <div className="left-section">
-          <div className="input-user-img">
-            <label htmlFor="inputImg" className="labelImg">
-              <img src={Userimg} alt={name} />
-            </label>
-            <input
-              className="inputImg"
-              id="inputImg"
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              style={{display:"none"}}
-            />
-           
-          </div>
-        </div>
+    <div className="w-3/4  mx-auto flex flex-col justify-center 
+    items-center">
+     
+        <label htmlFor="inputImg" className="">
+          <img src={Userimg} alt={name} className="w-36 h-36 rounded-full" />
+        </label>
+        <input
+          id="inputImg"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          style={{ display: "none" }}
+        />
+      
 
-        <div className="right-section">
-          <form action="" className="update-form">
-            <input
-              type="text"
-              placeholder="Enter your number"
-              value={name}
-              onChange={(e) => {
-                setname(e.target.value);
-              }}
-            />
-            <input
-              type="text"
-              placeholder="Enter your bio"
-              value={bio}
-              onChange={(e) => {
-                setbio(e.target.value);
-              }}
-            />
-            <button type="submit" className="submit-btn" onClick={submithandeler}>
-              Submit
-            </button>
-          </form>
-          <button className="submit-btn delet-btn" onClick={deleteaccounthandeler}>
-            Delete account
-            </button>
-        </div>
+      <div className="w-full flex flex-col items-center">
+        <form action="" className="flex flex-col w-full  mx-auto gap-2 items-center">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => {
+              setname(e.target.value);
+            }}
+            className="w-full h-10 bg-form-bg border-2 rounded-lg lg:w-3/5"
+          />
+          <textarea
+            type="text"
+            placeholder="Enter your bio"
+            value={bio}
+            onChange={(e) => {
+              setbio(e.target.value);
+              
+            }}
+            className="w-full h-20 bg-form-bg border-2 rounded-lg lg:w-3/5"
+          />
+          <button type="submit" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm py-2.5 text-center me-2 mb-2 w-40 " onClick={submithandeler}>
+            Submit
+          </button>
+        </form>
+        <button
+          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-52"
+          onClick={deleteaccounthandeler}
+        >
+          Delete account
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "form-bg":"#e6f3ff"
+      }
+    },
   },
   plugins: [],
 }
